e6fab565aa0020c86cb650881241ca8058395cd6
ishaan <ishaan@aleph-labs.com>
2019-08-07 16:40:31 +0800

    test commit

--- app/src/main/java/com/example/todo/Todo.kt
+++ app/src/main/java/com/example/todo/Todo.kt
@@ -2,4 +2,7 @@ package com.example.todo
 
 import java.time.LocalDateTime
 
+/**
+ * Data model class
+ */
 data class Todo(val id: Long, val description: String, val createdAt: LocalDateTime, val status: TodoStatus)
